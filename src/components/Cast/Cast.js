2622b0f446e8ea97b65b1e1da20176dba0072847
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-06-25 14:14:27 +0300

    +

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMoviesCredits } from 'components/service/service';
 import styles from 'components/Cast/styles.css';
+import imageNotFound from 'components/images/Image_not_available.png';
 
 const Cast = () => {
   const { movieId } = useParams();
@@ -37,6 +38,10 @@ const Cast = () => {
                 src={`https://image.tmdb.org/t/p/w300${castItem.profile_path}`}
                 alt={`${castItem.name} портрет`}
                 className={styles.castImage}
+                onError={(e) => {
+                  e.target.onerror = null; 
+                  e.target.src = imageNotFound;
+                }} 
               />
               <div className='castInfo'>
                 <p className='castName'>Ім'я: {castItem.name}</p>
